b37c5e93626fa1da1c5ec7cd22e6a79531f208c7
Artem <dzaparidzeartem@gmail.com>
2025-09-25 16:13:40 +0500

    feat: implement navigation between Schedule and Settings screens
    Added composable implementations for ScheduleScreen and SettingsScreen. Updated MainScreen to use a bottom navigation bar for switching between these screens, replacing the previous placeholder content.

--- app/src/main/java/ru/dzhaparidze/collegeapp/features/schedule/views/ScheduleScreen.kt
+++ app/src/main/java/ru/dzhaparidze/collegeapp/features/schedule/views/ScheduleScreen.kt
@@ -1,1 +1,36 @@
 package ru.dzhaparidze.collegeapp.features.schedule.views
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ScheduleScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            Text(
+                text = "Расписание",
+                style = MaterialTheme.typography.titleLarge,
+            )
+        }
+    ) { paddings ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddings),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Расписание",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
--- app/src/main/java/ru/dzhaparidze/collegeapp/features/shared/ui/screens/MainScreen.kt
+++ app/src/main/java/ru/dzhaparidze/collegeapp/features/shared/ui/screens/MainScreen.kt
@@ -7,36 +7,30 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import ru.dzhaparidze.collegeapp.features.schedule.views.ScheduleScreen
+import ru.dzhaparidze.collegeapp.features.schedule.views.SettingsScreen
 
+enum class Screen { SETTINGS, SCHEDULE }
 @Composable
 fun MainScreen() {
+    val currentScreen = remember { mutableStateOf(Screen.SCHEDULE) }
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         bottomBar = {
             NavigationBar {
-                // TODO: добавить логику выбранной вкладки
+                // РАСПИСАНИЕ
                 NavigationBarItem(
-                    onClick = { },
-                    icon = {
-                        Icon(
-                            Icons.Default.Settings,
-                            "SettingIcon"
-                        )
-                    },
-                    label = { Text("Настройки") },
-                    selected = false
-                )
-
-                NavigationBarItem(
-                    onClick = { },
+                    onClick = { currentScreen.value = Screen.SCHEDULE},
                     icon = {
                         Icon(
                             Icons.Default.DateRange,
@@ -44,7 +38,20 @@ fun MainScreen() {
                         )
                     },
                     label = { Text("Расписание") },
-                    selected = true
+                    selected = currentScreen.value == Screen.SCHEDULE,
+                )
+
+                // НАСТРОЙКИ
+                NavigationBarItem(
+                    onClick = { currentScreen.value = Screen.SETTINGS},
+                    icon = {
+                        Icon(
+                            Icons.Default.Settings,
+                            "SettingIcon"
+                        )
+                    },
+                    label = { Text("Настройки") },
+                    selected = currentScreen.value == Screen.SETTINGS,
                 )
             }
         }
@@ -54,11 +61,12 @@ fun MainScreen() {
                 .fillMaxSize()
                 .padding(paddings),
             contentAlignment = Alignment.Center
+
         ) {
-            Text(
-                text = "Главная",
-                style = MaterialTheme.typography.headlineMedium
-            )
+            when (currentScreen.value) {
+                Screen.SETTINGS -> SettingsScreen()
+                Screen.SCHEDULE -> ScheduleScreen()
+            }
         }
     }
 }
