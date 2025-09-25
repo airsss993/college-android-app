8128d5c28676efcc93225e4052dac0ce039cf262
Artem <dzaparidzeartem@gmail.com>
2025-09-25 21:25:45 +0500

    feat: add generic HTTP GET and schedule API integration
    Introduced a generic send function in HttpClient for making GET requests and decoding JSON responses. Added Endpoint data class for request configuration. Implemented ScheduleAPI to fetch schedules using the new HTTP client abstraction.

--- app/src/main/java/ru/dzhaparidze/collegeapp/core/networking/HttpClient.kt
+++ app/src/main/java/ru/dzhaparidze/collegeapp/core/networking/HttpClient.kt
@@ -5,7 +5,12 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpMethod
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
 
 object HttpClient {
     val instance = HttpClient(CIO) {
@@ -20,4 +25,26 @@ object HttpClient {
             connectTimeoutMillis = 15000
         }
     }
+
+    suspend inline fun <reified T> send(
+        endpoint: Endpoint,
+        baseUrl: String
+    ): T {
+        val response: HttpResponse = instance.get(baseUrl + endpoint.path) {
+            url {
+                endpoint.queryParams.forEach { (key, value) ->
+                    parameters.append(key, value)
+                }
+            }
+        }
+
+        val jsonString = response.bodyAsText()
+        return Json.decodeFromString<T>(jsonString)
+    }
 }
+
+data class Endpoint(
+    val path: String,
+    val method: HttpMethod,
+    val queryParams: Map<String, String>,
+)
--- app/src/main/java/ru/dzhaparidze/collegeapp/features/schedule/data/ScheduleApi.kt
+++ app/src/main/java/ru/dzhaparidze/collegeapp/features/schedule/data/ScheduleApi.kt
@@ -1,1 +1,39 @@
 package ru.dzhaparidze.collegeapp.features.schedule.data
+
+import io.ktor.http.HttpMethod
+import ru.dzhaparidze.collegeapp.core.networking.Endpoint
+import ru.dzhaparidze.collegeapp.core.networking.HttpClient.send
+import ru.dzhaparidze.collegeapp.features.schedule.models.ScheduleResponse
+import ru.dzhaparidze.collegeapp.features.schedule.utils.DateFormatters
+
+interface ScheduleAPIInterface {
+    suspend fun fetchSchedule(
+        group: String,
+        subgroup: String,
+        start: String,
+        end: String
+    ): ScheduleResponse
+}
+
+class ScheduleAPI : ScheduleAPIInterface {
+    override suspend fun fetchSchedule(
+        group: String,
+        subgroup: String,
+        start: String,
+        end: String
+    ): ScheduleResponse {
+        val endpoint = Endpoint(
+            path = "/api/v1/schedule",
+            method = HttpMethod.Get,
+            queryParams = mapOf(
+                "group" to group,
+                "subgroup" to subgroup,
+                "start" to DateFormatters.request.format(start),
+                "end" to DateFormatters.request.format(end)
+            ),
+        )
+
+        return send(endpoint, "http://localhost:8500")
+    }
+}
+
